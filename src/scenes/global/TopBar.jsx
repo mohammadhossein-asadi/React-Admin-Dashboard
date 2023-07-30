83335e391bab40881569a49312c6a567a6f28acc
mohammadhossein-asadi <mohammadhosseinasadi.p@gmail.com>
2023-07-30 18:02:24 +0330

    Making the sidebar component and writing different routes for other sections inside the sidebar and making their components

--- src/scenes/global/TopBar.jsx
+++ src/scenes/global/TopBar.jsx
@@ -1,5 +1,55 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlined from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlined from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlined from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlined from "@mui/icons-material/SettingsOutlined";
+import PersonOutlined from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
 const TopBar = () => {
-  return <div>TopBar</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display={"flex"} justifyContent={"space-between"} p={2}>
+      {/* SEARCH BAR */}
+      <Box
+        display={"flex"}
+        backgroundColor={colors.primary[400]}
+        borderRadius={"3px"}
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      {/* ICONS */}
+
+      <Box display={"flex"}>
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlined />
+          ) : (
+            <LightModeOutlined />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlined />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlined />
+        </IconButton>
+        <IconButton>
+          <PersonOutlined />
+        </IconButton>
+      </Box>
+    </Box>
+  );
 };
 
 export default TopBar;
